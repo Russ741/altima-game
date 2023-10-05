bfb86e88108cd868e0b8b92163aa3398e13b043e
Russ741 <72242656+Russ741@users.noreply.github.com>
2023-10-05 15:45:51 -0400

    Add smashed Altima asset, and add timer to trigger the movement and new adversary.

--- altima_game/lib/main.dart
+++ altima_game/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,14 +30,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _smashed = false;
   double _left = 300;
   double _top = 200;
 
+  Timer? _smashedTimer;
+
+  final NetworkImage _intactAltima = NetworkImage('https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT1E6kYE5_d9uoMa9N8cUPCsLpcM4RNss-wwzFsqqfmOfGFUJ9-');
+  final AssetImage _smashedAltima = AssetImage('images/smashed-altima.jpeg');
+
   void _clickCar() {
-    setState(() {
-      _left += 50;
-      _top += 50;
-    });
+    if (! _smashed) {
+      setState(() {
+        _smashed = true;
+      });
+      _smashedTimer = Timer(
+        const Duration(milliseconds: 300),
+        () {
+          setState(() {
+            _smashed = false;
+            _left += 30;
+            _top += 30;
+          });
+        },
+      );
+    }
   }
 
   @override
@@ -46,11 +65,11 @@ class _MyHomePageState extends State<MyHomePage> {
           top: _top,
           left: _left,
           child: GestureDetector(
-            child: Image.network(
-                'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT1E6kYE5_d9uoMa9N8cUPCsLpcM4RNss-wwzFsqqfmOfGFUJ9-',
+            onTap: _clickCar,
+            child: Image(
+                image: _smashed ? _smashedAltima : _intactAltima as ImageProvider<Object>,
                 height: 200,
                 width: 200),
-                onTap: _clickCar,
           ),
         )
       ],
